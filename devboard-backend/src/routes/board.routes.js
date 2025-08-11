6816c9abb5fb942ab33666989628a7bd79b77a64
saksham gupta <guptasaksham2510@gmail.com>
2025-08-11 19:15:02 +0530

    Remove member management routes from board.routes.js

--- devboard-backend/src/routes/board.routes.js
+++ devboard-backend/src/routes/board.routes.js
@@ -2,9 +2,6 @@ import express from "express";
 import {
   createBoard,
   getMyBoards,
-  getMembers,
-  addMember,
-  removeMember,
 } from "../controllers/board.controller.js";
 import { authMiddleware } from "../middleware/auth.middleware.js";
 import {
@@ -18,11 +15,6 @@ const router = express.Router();
 
 router.post("/", authMiddleware, createBoard);
 router.get("/", authMiddleware, getMyBoards);
-
-// Member management
-router.get("/:boardId/members", authMiddleware, getMembers); // GET members
-router.post("/:boardId/members", authMiddleware, addMember); // POST add member (body: email OR userId)
-router.delete("/:boardId/members", authMiddleware, removeMember); // DELETE remove member (body: email OR userId)
 
 router.put("/:boardId", authMiddleware, updateBoard);
 router.delete("/:boardId", authMiddleware, deleteBoard);
