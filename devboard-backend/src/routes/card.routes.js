6549c7ba1fc1b1c31f9e39952e19ef5de8b05198
saksham gupta <guptasaksham2510@gmail.com>
2025-08-12 14:20:14 +0530

    Refactor card routes to include additional card management functionalities and improve code organization

--- devboard-backend/src/routes/card.routes.js
+++ devboard-backend/src/routes/card.routes.js
@@ -1,13 +1,36 @@
 import express from 'express';
-import { createCard, getCardsByList, updateCard, deleteCard } from '../controllers/card.controller.js';
+
+import {
+  createCard,
+  getCardsByList,
+  updateCard,
+  deleteCard,
+  assignMemberToCard,
+  unassignMemberFromCard,
+  reorderCards,
+  moveCard
+} from '../controllers/card.controller.js';
 import { authMiddleware } from '../middleware/auth.middleware.js';
+import { getCardsByBoard } from '../controllers/card.controller.js';
+
 
 const router = express.Router();
 
 router.post('/', authMiddleware, createCard);
-router.get('/:listId', authMiddleware, getCardsByList); 
+router.get('/:listId', authMiddleware, getCardsByList);
 router.put('/:cardId', authMiddleware, updateCard);
 router.delete('/:cardId', authMiddleware, deleteCard);
+router.patch("/:cardId/move", moveCard);
+
+router.get('/board/:boardId', authMiddleware, getCardsByBoard);  
+
+// Assign/unassign users
+router.post('/:cardId/assign', authMiddleware, assignMemberToCard);
+router.post('/:cardId/unassign', authMiddleware, unassignMemberFromCard);
+
+// Reorder cards
+router.put('/reorder/:listId', authMiddleware, reorderCards);
+
 
 
 export default router;
